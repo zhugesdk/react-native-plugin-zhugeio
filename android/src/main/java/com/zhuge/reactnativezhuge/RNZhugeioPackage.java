53ac0cb4f8b101c3d2dbaa66e2cbbcc7a1351dc3
songf <smileysun1994@gmail.com>
2020-12-22 15:29:43 +0800

    update version 1.1.7 update gradle file

--- android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
+++ android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class RNZhugeioPackage implements ReactPackage {
 
-    public static final String VERSION = "1.1.6";
+    public static final String VERSION = "1.1.7";
 
 
     @Override

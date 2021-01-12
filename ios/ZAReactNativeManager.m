2037631c7406ea826ba33f16920dadcc0bd42116
songf <smileysun1994@gmail.com>
2021-01-12 17:31:10 +0800

    更新iOS端 $page_title 字段

--- ios/ZAReactNativeManager.m
+++ ios/ZAReactNativeManager.m
@@ -18,7 +18,7 @@
 
 #pragma mark - Constants
 NSString *const kZAEventScreenNameProperty = @"$screen_name";
-NSString *const kZAEventTitleProperty = @"$title";
+NSString *const kZAEventTitleProperty = @"$page_title";
 NSString *const kZAEventElementContentProperty = @"$element_content";
 
 

ee39dd30d7cd3bf978275518e063fe20b1fc950c
KangZhu <zhukang0613@163.com>
2021-11-15 16:31:32 +0800

    Revert "安卓引用库使用最新版本"

--- android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
+++ android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class RNZhugeioPackage implements ReactPackage {
 
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.1.8";
 
 
     @Override

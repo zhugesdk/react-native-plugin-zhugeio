c867044f60f5bda36698e444fdadc15d2bc89d59
zhukang <zhukang@zhugeio.com>
2021-11-26 11:31:26 +0800

    版本更新

--- android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
+++ android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class RNZhugeioPackage implements ReactPackage {
 
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "2.0.0";
 
 
     @Override

c7cc4072dcb21c2afd44033858e15f2d357ae05a
jiaokang <jiaokang@amberweather.com>
2024-11-14 16:00:06 +0800

    version 2.0.4

--- android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
+++ android/src/main/java/com/zhuge/reactnativezhuge/RNZhugeioPackage.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class RNZhugeioPackage implements ReactPackage {
 
-    public static final String VERSION = "2.0.3";
+    public static final String VERSION = "2.0.4";
 
 
     @Override
